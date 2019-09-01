1da472a4a0d087d08913586cbb7a2f4945bce240
EdgarAllanPro <searchjobs938@.gmail.com>
2019-09-01 21:31:50 +0300

    Add 'empty_scope()' because for earlier versions of python contextlib.nullcontext is not available

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
+import contextlib
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras.layers import Dense, Wrapper
 from tensorflow.python.keras import backend as K
 import tensorflow_probability.python.distributions as tfd
+
+
+@contextlib.contextmanager
+def empty_context_mgr():
+    yield None
 
 
 def clip_value(a, a_min, a_max):
